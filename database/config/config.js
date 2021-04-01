69efab5f42665e6d309970beec6c89e3ea474841
devganesh1998 <devganesh91@gmail.com>
2021-04-01 22:27:51 +0530

    added db connection pool options

--- database/config/config.js
+++ database/config/config.js
@@ -2,9 +2,19 @@ module.exports = {
   development: {
     url: process.env.DEV_DATABASE_URL,
     dialect: "postgres",
+    pool: {
+      max: 6,
+      min: 1,
+      acquire: 30000,
+    }
   },
   production: {
     url: process.env.PROD_DATABASE_URL,
     dialect: "postgres",
+    pool: {
+      max: 6,
+      min: 1,
+      acquire: 30000,
+    }
   },
 };
