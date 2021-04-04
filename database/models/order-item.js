8f16c8d5b0a95df1d4ac2d199b5deb3be5b3caae
devganesh1998 <devganesh91@gmail.com>
2021-04-04 15:16:12 +0530

    item-order db model

---
+++ database/models/order-item.js
@@ -0,0 +1,46 @@
+import orders from './orders';
+import items from './items';
+import addresses from './addresses';
+
+module.exports = (sequelize, DataTypes) => {
+    const orderItem = sequelize.define(
+        'order-item',
+        {
+            orderItemId: {
+                allowNull: false,
+                primaryKey: true,
+                unique: true,
+                validate: { isUUID: 4 },
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4,
+            },
+            orderId: {
+                allowNull: false,
+                validate: { isUUID: 4 },
+                type: DataTypes.UUID,
+            },
+            itemId: {
+                allowNull: false,
+                validate: { isUUID: 4 },
+                type: DataTypes.UUID,
+            },
+            pickupLocation: {
+                allowNull: false,
+                validate: { isUUID: 4 },
+                type: DataTypes.UUID,
+            },
+            quantity: {
+                allowNull: false,
+                type: DataTypes.INTEGER,
+            },
+        },
+        {
+            timestamps: true,
+            tableName: 'order-item',
+        },
+    );
+    orderItem.belongsTo(orders, { foreignKey: 'addressId' });
+    orderItem.belongsTo(items, { foreignKey: 'itemId' });
+    orderItem.belongsTo(addresses, { foreignKey: 'pickupLocation' });
+    return orderItem;
+};
