59519206bbfd9c0beb1527fdf6f079933da90106
devganesh1998 <devganesh91@gmail.com>
2021-04-04 11:35:20 +0530

    eslint --fix

--- database/config/config.js
+++ database/config/config.js
@@ -1,20 +1,20 @@
 module.exports = {
-  development: {
-    url: process.env.DEV_DATABASE_URL,
-    dialect: "postgres",
-    pool: {
-      max: 6,
-      min: 1,
-      acquire: 30000,
-    }
-  },
-  production: {
-    url: process.env.PROD_DATABASE_URL,
-    dialect: "postgres",
-    pool: {
-      max: 6,
-      min: 1,
-      acquire: 30000,
-    }
-  },
+    development: {
+        url: process.env.DEV_DATABASE_URL,
+        dialect: 'postgres',
+        pool: {
+            max: 6,
+            min: 1,
+            acquire: 30000,
+        },
+    },
+    production: {
+        url: process.env.PROD_DATABASE_URL,
+        dialect: 'postgres',
+        pool: {
+            max: 6,
+            min: 1,
+            acquire: 30000,
+        },
+    },
 };
--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const morgan = require("morgan");
-const db = require("./database/models");
-const { PORT, isDev, ALLOWED_ORIGINS } = require("./config");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const db = require('./database/models');
+const { PORT, isDev, ALLOWED_ORIGINS } = require('./config');
 
 const app = express();
 app.use(cookieParser());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (isDev) {
-  const cors = require("cors");
-  app.use(cors());
+    // eslint-disable-next-line global-require,import/no-extraneous-dependencies
+    const cors = require('cors');
+    app.use(cors());
 } else {
-  app.use(function (req, res, next) {
-    const origin = req.headers.origin;
-    if (ALLOWED_ORIGINS.indexOf(origin) > -1) {
-      res.setHeader("Access-Control-Allow-Origin", origin);
-    }
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, cookie"
-    );
-    res.header("Access-Control-Allow-Credentials", true);
-    next();
-  });
+    app.use(function (req, res, next) {
+        const { origin } = req.headers;
+        if (ALLOWED_ORIGINS.indexOf(origin) > -1) {
+            res.setHeader('Access-Control-Allow-Origin', origin);
+        }
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, cookie');
+        res.header('Access-Control-Allow-Credentials', true);
+        next();
+    });
 }
 
-app.get("/info", (req, res) => {
-  res.send("Welcome to Order Anything server");
+app.get('/info', (req, res) => {
+    res.send('Welcome to Order Anything server');
 });
 
 // const apiRoutes = require("./app/routes");
@@ -39,20 +37,20 @@ app.get("/info", (req, res) => {
 // app.use("/", apiRoutes);
 
 (async () => {
-  let retries = 5;
-  while (retries) {
-    try {
-      await db.sequelize.sync();
-      app.listen(PORT, () => {
-        console.log(`listening on: http://localhost:${PORT}`);
-      });
-      break;
-    } catch (err) {
-      console.error(err);
-      retries -= 1;
-      console.log(`retries left: ${retries}`);
-      // wait 5 seconds
-      await new Promise((res) => setTimeout(res, 5000));
+    let retries = 5;
+    while (retries) {
+        try {
+            await db.sequelize.sync();
+            app.listen(PORT, () => {
+                console.log(`listening on: http://localhost:${PORT}`);
+            });
+            break;
+        } catch (err) {
+            console.error(err);
+            retries -= 1;
+            console.log(`retries left: ${retries}`);
+            // wait 5 seconds
+            await new Promise((res) => setTimeout(res, 5000));
+        }
     }
-  }
 })();
