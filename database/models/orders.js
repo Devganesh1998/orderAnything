61ababd9337d132d00c9859bfddbf217533959b7
devganesh1998 <devganesh91@gmail.com>
2021-04-04 15:07:24 +0530

    added orders db model

---
+++ database/models/orders.js
@@ -0,0 +1,47 @@
+import { ORDER_STAGE, PAYMENT_MODE } from '../../config';
+import customers from './customers';
+import deliveryPerson from './deliveryPerson';
+
+module.exports = (sequelize, DataTypes) => {
+    const orders = sequelize.define(
+        'orders',
+        {
+            orderId: {
+                allowNull: false,
+                primaryKey: true,
+                unique: true,
+                type: DataTypes.UUID,
+                validate: { isUUID: 4 },
+                defaultValue: DataTypes.UUIDV4,
+            },
+            dropAddress: {
+                allowNull: false,
+                type: DataTypes.STRING,
+            },
+            stage: {
+                allowNull: false,
+                type: DataTypes.ENUM(Object.keys(ORDER_STAGE)),
+            },
+            paymentMode: {
+                allowNull: false,
+                type: DataTypes.ENUM(Object.keys(PAYMENT_MODE)),
+            },
+            lat: {
+                allowNull: false,
+                type: DataTypes.FLOAT,
+            },
+            long: {
+                allowNull: false,
+                type: DataTypes.FLOAT,
+            },
+        },
+        {
+            tableName: 'orders',
+            timestamps: true,
+            indexes: [{ fields: ['stage', 'paymentMode'] }],
+        },
+    );
+    orders.belongsTo(customers, { foreignKey: 'customerId', allowNull: false });
+    orders.belongsTo(deliveryPerson, { foreignKey: 'deliveryPersonId', allowNull: false });
+    return orders;
+};
